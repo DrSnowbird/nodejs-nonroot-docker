ecdafa98ba45080aed446c80f903028c0fc3df98
DrSnowbird <DrSnowbird@openkbs.org>
2023-01-09 10:08:25 -0500

    updated/fixed: docker-compose generator to auto-convert name to all-lowercase

--- run.sh
+++ run.sh
@@ -14,6 +14,10 @@ if [ $# -lt 1 ]; then
     echo "--------------------------------------------------------"
 fi
 
+if [ ! -s .env ]; then
+    bin/auto-config-all.sh
+fi
+
 ###########################################################################
 #### ---- RUN Configuration (CHANGE THESE if needed!!!!)           --- ####
 ###########################################################################
@@ -446,7 +450,7 @@ function checkHostVolumePath() {
     _SYS_PATHS="/dev /var /etc"
     if [[ $_SYS_PATHS != *"${_left}"* ]]; then
         sudo chown -R ${USER_ID}:${USER_ID} ${_left}
-        ls -al ${_left}
+        #ls -al ${_left}
     fi
 }
 
